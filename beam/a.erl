447069e5ddbc44abfadd6f3181f07b2a598796bc
Viktor Gergely <lustalista@gmail.com>
2019-10-03 10:05:51 +0200

    [add] sending example

--- beam/a.erl
+++ beam/a.erl
@@ -1,6 +1,6 @@
 -module(a).
 
--export([call/1, test/1, newproc/1, list/0, listlength/1]).
+-export([call/1, test/1, newproc/1, list/0, listlength/1, sending/0]).
 
 call(_) ->
     b:call().
@@ -18,3 +18,7 @@ listlength([_ | T]) ->
     1 + listlength(T);
 listlength([]) ->
     0.
+
+sending() ->
+    Pid = spawn(fun() -> receive X -> X end end),
+    Pid ! message.
